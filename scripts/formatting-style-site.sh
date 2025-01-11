4f4302c5b898ba1ee87e13c2771e8d497ce1b08a
Paul J Thordarson <kapunga@gmail.com>
2025-01-11 14:57:34 -0500

    Add new workflow to enforce formatting and site

---
+++ scripts/formatting-style-site.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+set -o xtrace
+
+sbt scalafmtAll; sbt scalafixAll; sbt laikaSite
+
+MODIFIED=$(git ls-files --modified --exclude-standard)
+if [[ -n "$MODIFIED" ]]; then
+  echo "ERROR: Formatting and publish changed some files."
+  echo "$MODIFIED"
+  exit 1
+fi
