53405879d0e1629929b0c557583029aea85d0d44
Paul J Thordarson <kapunga@gmail.com>
2025-02-03 08:14:44 -0500

    Add license headers to everything

--- scripts/formatting-style-site.sh
+++ scripts/formatting-style-site.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 set -o xtrace
 
-sbt scalafmtAll; sbt scalafixAll; sbt laikaSite; sbt headerCheck
+sbt scalafmtAll; sbt scalafixAll; sbt laikaSite; sbt headerCheckAll
 
 MODIFIED=$(git ls-files --modified --exclude-standard)
 if [[ -n "$MODIFIED" ]]; then
