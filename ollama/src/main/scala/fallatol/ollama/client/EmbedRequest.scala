0d11dd80ee82d822d6e0dcfe4fffe54310ecee9c
Paul J Thordarson <kapunga@gmail.com>
2025-02-02 23:30:38 -0500

    Format

--- ollama/src/main/scala/fallatol/ollama/client/EmbedRequest.scala
+++ ollama/src/main/scala/fallatol/ollama/client/EmbedRequest.scala
@@ -6,7 +6,8 @@ import io.circe.Codec
 case class EmbedRequest(model: Model, input: Seq[String])
 
 object EmbedRequest {
-  def apply(model: Model, input: String): EmbedRequest = EmbedRequest(model, Seq(input))
+  def apply(model: Model, input: String): EmbedRequest =
+    EmbedRequest(model, Seq(input))
 
   implicit val codec: Codec[EmbedRequest] = EmbedRequestCodec.codec
 }
--- ollama/src/main/scala/fallatol/ollama/client/EmbedResponse.scala
+++ ollama/src/main/scala/fallatol/ollama/client/EmbedResponse.scala
@@ -3,7 +3,13 @@ package fallatol.ollama.client
 import fallatol.ollama.Model
 import io.circe.Codec
 
-case class EmbedResponse(model: Model, embeddings: Seq[Seq[Double]], totalDuration:Long, loadDuration: Long, promptEvalCount: Long)
+case class EmbedResponse(
+    model: Model,
+    embeddings: Seq[Seq[Double]],
+    totalDuration: Long,
+    loadDuration: Long,
+    promptEvalCount: Long
+)
 
 object EmbedResponse {
   implicit val codec: Codec[EmbedResponse] = EmbedResponseCodec.codec
