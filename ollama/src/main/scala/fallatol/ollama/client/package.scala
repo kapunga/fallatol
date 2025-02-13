c8e69da96407f5372e583c217a7c28357f3194bf
Paul J Thordarson <kapunga@gmail.com>
2025-02-12 19:15:19 -0500

    Fixed Scala Steward Merge conflict

--- ollama/src/main/scala/fallatol/ollama/client/package.scala
+++ ollama/src/main/scala/fallatol/ollama/client/package.scala
@@ -21,11 +21,10 @@
 
 package fallatol.ollama
 
-import io.circe
 import sttp.client4._
 
 package object client {
   type OllamaRequest[A] =
-    Request[Either[ResponseException[String, circe.Error], A]]
+    Request[Either[ResponseException[String], A]]
   type SyncType[A] = Either[Throwable, A]
 }
